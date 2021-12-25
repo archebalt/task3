746e2dec97cc4ce2d2c2c70c182edbf60798d419
Константин Болотовский <kosty.bolotovsky@gmail.com>
2021-12-25 22:11:00 +0300

    Create tests

---
+++ src/test/kotlin/task3/OrderTest.kt
@@ -0,0 +1,22 @@
+package task3
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class OrderTest
+{
+
+    @Test
+    fun getTotalPriceTest()
+    {
+        val ordPrice = Order(DeliveryMethod.CourierDelivery("home",2000.0), ItemEntry(Item("phone",20000.0), 3),10.0).getTotalPrice()
+        assertEquals(56000,ordPrice)
+    }
+
+    @Test
+    fun getTotalPriceTest2()
+    {
+        val ordPrice1 = Order(DeliveryMethod.SelfPickup, ItemEntry(Item("computer",35000.0), 2),0.0).getTotalPrice()
+        assertEquals(70000,ordPrice1)
+    }
+}
