a036cd887304f609296b4323235cae68591f8ab3
Artur <arturha13@gmail.com>
2022-01-25 02:45:40 +0300

    corrections

--- src/main/kotlin/lab1/align.kt
+++ src/main/kotlin/lab1/align.kt
@@ -1,13 +1,13 @@
 package lab1
 
 enum class Alignment {
-    LEFT ,
-    RIGHT ,
-    CENTER ,
+    LEFT,
+    RIGHT,
+    CENTER,
     WIDTH
 }
 
-fun createStringWithRequiredWidth(withoutSpaces : MutableList<String> , lineWidth : Int = 120) : String {
+private fun createStringWithRequiredWidth(withoutSpaces: MutableList<String>, lineWidth: Int = 120): String {
     var remainWidth = lineWidth
     var res = ""
     var i = 0
@@ -20,40 +20,73 @@ fun createStringWithRequiredWidth(withoutSpaces : MutableList<String> , lineWidt
             res += withoutSpaces[i] + '\n'
             remainWidth = lineWidth
             i++
-        } else {
-            res += '\n'
-            remainWidth = lineWidth
-        }
-    }
-    return res
-}
-
-fun createWidthAlignString(withoutSpaces : MutableList<String> , lineWidth : Int = 120) : String {
-    var remainWidth = lineWidth
-    var res = ""
-    var i = 0
-    while (i < withoutSpaces.size) {
-        if (withoutSpaces[i].length < remainWidth) {
-            res += withoutSpaces[i] + " "
-            remainWidth -= (withoutSpaces[i].length + 1)
-            i++
-        } else if (withoutSpaces[i].length == remainWidth) {
-            res += withoutSpaces[i] + '\n'
-            remainWidth = lineWidth
-            i++
-        } else {
+        } else if (withoutSpaces[i].length > lineWidth && lineWidth == remainWidth) {
             for (e in 0 until remainWidth) {
                 res += withoutSpaces[i][e]
             }
             res += '\n'
             withoutSpaces[i] = withoutSpaces[i].substring(remainWidth)
             remainWidth = lineWidth
+        } else {
+            res += '\n'
+            remainWidth = lineWidth
         }
     }
     return res
 }
 
-fun createCenterAlignString(requiredStringWidth : String , lineWidth : Int) : String {
+private fun createWidthAlignString(requiredStringWidth: String, lineWidth: Int): String {
+    var widthAlign = ""
+    var i = 0
+    while (i != requiredStringWidth.length) {
+        var countSymbolsEachString = 0
+        var eachString = ""
+        var countProbels = 0
+        while (requiredStringWidth[i] != '\n' && i < requiredStringWidth.length - 1) {
+            if (requiredStringWidth[i] != ' ') {
+                countSymbolsEachString++
+                eachString += requiredStringWidth[i]
+            } else if (requiredStringWidth[i + 1] != '\n') {
+                countSymbolsEachString++
+                eachString += requiredStringWidth[i]
+                countProbels++
+            }
+            i++
+            if (i == requiredStringWidth.length)
+                break
+        }
+        val probels = lineWidth - countSymbolsEachString
+        var numProbel = 0
+        var del = 0
+        if (countProbels != 0) {
+            del = probels / countProbels
+        }
+        var ostDel = 0
+        if (countProbels != 0) {
+            ostDel = probels % countProbels
+        }
+        for (n in 0 until eachString.length) {
+            if (eachString[n] != ' ') {
+                widthAlign += eachString[n]
+            }
+            if (eachString[n] == ' ') {
+                widthAlign += eachString[n]
+                numProbel++
+                for (l in 0 until del) {
+                    widthAlign += " "
+                }
+                if (ostDel >= numProbel) {
+                    widthAlign += " "
+                }
+            }
+        }
+        widthAlign += '\n'
+        i++
+    }
+    return widthAlign
+}
+
+private fun createCenterAlignString(requiredStringWidth: String, lineWidth: Int): String {
     var centerAlign = ""
     var i = 0
     while (i != requiredStringWidth.length) {
@@ -86,7 +119,7 @@ fun createCenterAlignString(requiredStringWidth : String , lineWidth : Int) : St
     return centerAlign
 }
 
-fun createRightAlignString(requiredStringWidth : String , lineWidth : Int) : String {
+private fun createRightAlignString(requiredStringWidth: String, lineWidth: Int): String {
     var rightAlign = ""
     var i = 0
     while (i != requiredStringWidth.length) {
@@ -114,37 +147,25 @@ fun createRightAlignString(requiredStringWidth : String , lineWidth : Int) : Str
     return rightAlign
 }
 
-
 fun alignText(
-    text : String ,
-    lineWidth : Int = 120 ,
-    alignment : Alignment = Alignment.LEFT
-) : String {
-    val strElements = text.split(" " , "\n")
-    val withoutSpaces : MutableList<String> = mutableListOf()
+    text: String,
+    lineWidth: Int = 120,
+    alignment: Alignment = Alignment.LEFT
+): String {
+    if (lineWidth < 1)
+        throw Exception("длина меньше 1")
+    val strElements = text.split(" ", "\n")
+    val withoutSpaces: MutableList<String> = mutableListOf()
     for (i in 0 until strElements.size - 1) {
-        if (strElements[i].isNotBlank()) {
+        if (strElements[i].isNotBlank())
             withoutSpaces.add(strElements[i])
-        }
     }
-    val requiredStringWidth = createStringWithRequiredWidth(withoutSpaces , lineWidth)
-    var alignStr = ""
-
-    if (alignment == Alignment.LEFT) {
-        alignStr = requiredStringWidth
+    val requiredStringWidth = createStringWithRequiredWidth(withoutSpaces, lineWidth)
+    val alignStr = when (alignment) {
+        Alignment.LEFT -> requiredStringWidth
+        Alignment.WIDTH -> createWidthAlignString(requiredStringWidth, lineWidth)
+        Alignment.RIGHT -> createRightAlignString(requiredStringWidth, lineWidth)
+        Alignment.CENTER -> createCenterAlignString(requiredStringWidth, lineWidth)
     }
-
-    if (alignment == Alignment.WIDTH) {
-        alignStr = createWidthAlignString(withoutSpaces , lineWidth)
-    }
-
-    if (alignment == Alignment.RIGHT) {
-        alignStr = createRightAlignString(requiredStringWidth , lineWidth)
-    }
-
-    if (alignment == Alignment.CENTER) {
-        alignStr = createCenterAlignString(requiredStringWidth , lineWidth)
-    }
-
     return alignStr
 }
