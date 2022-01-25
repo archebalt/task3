af0b8779560b5e79dc01e37dc61268611b620d52
Константин Болотовский <kosty.bolotovsky@gmail.com>
2022-01-25 18:49:30 +0300

    Kosty

--- src/main/kotlin/lab3/Circle.kt
+++ src/main/kotlin/lab3/Circle.kt
@@ -3,19 +3,22 @@ package lab3
 import kotlin.math.PI
 import kotlin.system.exitProcess
 
-class Circle(private val r : Double) : Shape {
-    override fun calcArea() : Double {
-        if (r < 0) {
-            println("error")
-            exitProcess(400)
+class Circle(val r : Double) : Shape {
+
+    init {
+        if (r <= 0) {
+            throw IllegalArgumentException("Data entered incorrectly")
         }
+    }
+
+    override fun calcArea() : Double {
+
         return r * r * PI
     }
 
     override fun calcPerimeter() : Double {
         if (r < 0) {
-            println("error")
-            exitProcess(400)
+            throw Exception()
         }
         return 2 * r * PI
     }
--- src/main/kotlin/lab3/Triangle.kt
+++ src/main/kotlin/lab3/Triangle.kt
@@ -3,22 +3,21 @@ package lab3
 import kotlin.math.sqrt
 import kotlin.system.exitProcess
 
-class Triangle(private val a : Double , private val b : Double , private val c : Double) : Shape { /* implementation */
-    override fun calcArea() : Double {
-        if (a < 0 || b < 0 || c < 0) {
-            println("error")
-            exitProcess(400)
+class Triangle(val a : Double, val b : Double, val c : Double) : Shape {
+
+    init {
+        if (a <= 0 || b <= 0 || c <= 0 && !(a+b>c && c+b>a && c+a>b))  {
+            throw IllegalArgumentException("Data entered incorrectly")
         }
+    }
+
+    override fun calcArea() : Double {
         val perimeter = (a + b + c) / 2
         val area = perimeter * (perimeter - a) * (perimeter - b) * (perimeter - c)
         return sqrt(area)
     }
 
     override fun calcPerimeter() : Double {
-        if (a < 0 || b < 0 || c < 0) {
-            println("error")
-            exitProcess(400)
-        }
         return a + b + c
     }
 }
