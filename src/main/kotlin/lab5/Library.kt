6f8bfee5677f385002af7868ef1c842db34994c4
Artur <arturha13@gmail.com>
2022-01-25 21:22:43 +0300

    fix

--- src/main/kotlin/lab5/Library.kt
+++ src/main/kotlin/lab5/Library.kt
@@ -1,125 +1,119 @@
 package lab5
 
-import kotlin.collections.Map as Mapof
-
-data class Book(val title : String , val authors : List<Author> , val genre : Genre , val year : Int)
-data class Author(val name : String , val Surname : String)
-data class User(val name : String , val Surname : String)
+data class Book(val title: String, val authors: List<Author>, val genre: Genre, val year: Int)
+data class Author(val name: String, val Surname: String)
+data class User(val name: String, val Surname: String)
 
 enum class Genre {
-    HISTORY , FANTASY ,
+    HISTORY, FANTASY,
 }
 
 sealed class Status {
     object Available : Status()
-    data class UsedBy(val user : User) : Status()
+    data class UsedBy(val user: User) : Status()
     object ComingSoon : Status()
     object Restoration : Status()
 }
 
 interface LibraryService {
-    fun findBooks(substring : String) : List<Book>
-    fun findBooks(author : Author) : List<Book>
-    fun findBooks(year : Int) : List<Book>
-    fun findBooks(genre : Genre) : List<Book>
+    fun findBooks(substring: String): List<Book>
+    fun findBooks(author: Author): List<Book>
+    fun findBooks(year: Int): List<Book>
+    fun findBooks(genre: Genre): List<Book>
 
-    fun getAllBooks() : List<Book>
-    fun getAllAvailableBooks() : List<Book>
+    fun getAllBooks(): List<Book>
+    fun getAllAvailableBooks(): List<Book>
 
-    fun getBookStatus(book : Book) : Status
-    fun getAllBookStatuses() : Mapof<Book , Status>
+    fun getBookStatus(book: Book): Status?
+    fun getAllBookStatuses(): Map<Book, Status>
 
-    fun setBookStatus(book : Book , status : Status)
+    fun setBookStatus(book: Book, status: Status)
 
-    fun addBook(book : Book , status : Status = Status.Available)
+    fun addBook(book: Book, status: Status = Status.Available)
 
-    fun registerUser(user : User)
-    fun unregisterUser(user : User)
+    fun registerUser(user: User)
+    fun unregisterUser(user: User)
 
-    fun takeBook(user : User , book : Book)
-    fun returnBook(book : Book)
+    fun takeBook(user: User, book: Book)
+    fun returnBook(book: Book)
 
-    fun sendToRestoration(book : Book)
-    fun comingSoon(book : Book)
+    fun sendToRestoration(book: Book)
+    fun comingSoon(book: Book)
 }
 
 const val maxNumOfBooks = 3
 
-class Library(books : Mapof<Book , Status> , users : List<User>) : LibraryService {
-    private var libraryBooks = mutableMapOf<Book , Status>()
+class Library : LibraryService {
+    private var libraryBooks = mutableMapOf<Book, Status>()
     private var libraryUsers = mutableListOf<User>()
 
-    init {
-        libraryBooks.plusAssign(books)
-        libraryUsers.plusAssign(users)
-    }
 
-    override fun findBooks(substring : String) : List<Book> {
+    override fun findBooks(substring: String): List<Book> {
         return libraryBooks.keys.filter { book -> book.title.contains(substring) }.toList()
     }
 
-    override fun findBooks(author : Author) : List<Book> {
+    override fun findBooks(author: Author): List<Book> {
         return libraryBooks.keys.filter { book -> book.authors.contains(author) }.toList()
     }
 
-    override fun findBooks(genre : Genre) : List<Book> {
+    override fun findBooks(genre: Genre): List<Book> {
         return libraryBooks.keys.filter { book -> book.genre == genre }.toList()
     }
 
-    override fun findBooks(year : Int) : List<Book> {
+    override fun findBooks(year: Int): List<Book> {
         return libraryBooks.keys.filter { book -> book.year == year }.toList()
     }
 
-    override fun getAllBooks() : List<Book> {
+    override fun getAllBooks(): List<Book> {
         return libraryBooks.keys.toList()
     }
 
-    override fun getAllAvailableBooks() : List<Book> {
+    override fun getAllAvailableBooks(): List<Book> {
         return libraryBooks.filter { book -> book.value == Status.Available }.keys.toList()
     }
 
-    override fun getBookStatus(book : Book) : Status {
-        return libraryBooks.getValue(book)
+    override fun getBookStatus(book: Book): Status? {
+        return libraryBooks[book]
     }
 
-    override fun getAllBookStatuses() : Mapof<Book , Status> {
-        return libraryBooks
+    override fun getAllBookStatuses(): Map<Book, Status> {
+        return libraryBooks.toMap()
     }
 
-    override fun setBookStatus(book : Book , status : Status) {
+    override fun setBookStatus(book: Book, status: Status) {
         if (!libraryBooks.contains(book))
-            throw Exception("there is no such book")
+            throw IllegalArgumentException("there is no such book")
         libraryBooks[book] = status
     }
 
-    override fun addBook(book : Book , status : Status) {
+    override fun addBook(book: Book, status: Status) {
         libraryBooks[book] = status
     }
 
-    override fun registerUser(user : User) {
+    override fun registerUser(user: User) {
         if (libraryUsers.contains(user))
-            throw Exception("this user is already exists")
+            throw IllegalArgumentException("this user is already exists")
         libraryUsers.plusAssign(user)
     }
 
-    override fun returnBook(book : Book) {
+    override fun returnBook(book: Book) {
         libraryBooks[book] = Status.Available
     }
 
-    override fun takeBook(user : User , book : Book) {
+    override fun takeBook(user: User, book: Book) {
         if (libraryBooks.keys.contains(book) && libraryUsers.contains(user)) {
             if (libraryBooks.filter { book -> book.value == Status.UsedBy(user) }.size < maxNumOfBooks && libraryBooks[book] == Status.Available) {
                 libraryBooks[book] = Status.UsedBy(user)
             } else
-                throw Exception("user has on his hands 3 or more books or book isn't available")
+                throw IllegalArgumentException("user has on his hands 3 or more books or book isn't available")
         } else
-            throw Exception("there is no book or user")
+            throw IllegalArgumentException("there is no book or user")
 
     }
 
-    override fun unregisterUser(user : User) {
+    override fun unregisterUser(user: User) {
         if (!libraryUsers.contains(user))
-            throw Exception("there is no such user")
+            throw IllegalArgumentException("there is no such user")
         for (el in libraryBooks)
             if (el.value == Status.UsedBy(user))
                 libraryBooks[el.key] = Status.Available
@@ -127,17 +121,17 @@ class Library(books : Mapof<Book , Status> , users : List<User>) : LibraryServic
 
     }
 
-    override fun sendToRestoration(book : Book) {
+    override fun sendToRestoration(book: Book) {
         if (libraryBooks.keys.contains(book)) {
             if (libraryBooks[book] == Status.Available)
                 libraryBooks[book] = Status.Restoration
             else
-                throw Exception("book isn't available")
+                throw IllegalArgumentException("book isn't available")
         } else
-            throw Exception("there is no book")
+            throw IllegalArgumentException("there is no book")
     }
 
-    override fun comingSoon(book : Book) {
+    override fun comingSoon(book: Book) {
         libraryBooks[book] = Status.ComingSoon
     }
 }
--- src/main/kotlin/lab5/Main.kt
+++ src/main/kotlin/lab5/Main.kt
@@ -1,40 +1,41 @@
 package lab5
 
 fun main() {
-    val author1 = Author("artur" , "melnik")
+    val author1 = Author("artur", "melnik")
     val list1 = mutableListOf<Author>(author1)
-    val book1 = Book("harry potter" , list1 , Genre.FANTASY , 1997)
-    val user1 = User("petter" , "parker")
+    val book1 = Book("harry potter", list1, Genre.FANTASY, 1997)
+    val user1 = User("petter", "parker")
 
-    val author2 = Author("MR" , "BOB")
+    val author2 = Author("MR", "BOB")
     val list2 = mutableListOf<Author>(author2)
-    val book2 = Book("History of MR. BOB" , list2 , Genre.HISTORY , 0)
-    val user2 = User("Pavel" , "2")
-
-    val booksToConstructor = mapOf<Book , Status>(book1 to Status.Available , book2 to Status.Available)
-    val usersToConstructor = listOf<User>(user1 , user2)
-    val library1 = Library(booksToConstructor , usersToConstructor)
+    val book2 = Book("History of MR. BOB", list2, Genre.HISTORY, 0)
+    val user2 = User("Pavel", "2")
+    val library1 = Library()
+    library1.addBook(book1, Status.Available)
+    library1.addBook(book2, Status.Available)
+    library1.registerUser(user1)
+    library1.registerUser(user2)
     val findBooks = library1.findBooks(0)
     println("1. year = $findBooks")
 
-    val author3 = Author("Artem" , "Bodrov")
+    val author3 = Author("Artem", "Bodrov")
     val list3 = mutableListOf<Author>(author3)
-    val book3 = Book("Bro" , list3 , Genre.HISTORY , 1997)
-    library1.addBook(book3 , Status.UsedBy(user1))
+    val book3 = Book("Bro", list3, Genre.HISTORY, 1997)
+
+    library1.addBook(book3, Status.UsedBy(user1))
     val findBooks2 = library1.findBooks(Genre.HISTORY)
     println("2. History genre = $findBooks2")
 
     println("3. All books = ${library1.getAllBookStatuses()}")
 
-    library1.takeBook(user2 , book1)
+    library1.takeBook(user2, book1)
     println("4. All books = ${library1.getAllBookStatuses()}")
 
-    val user4 = User("Stepan" , "Suslov")
+    val user4 = User("Stepan", "Suslov")
     library1.registerUser(user4)
-    library1.takeBook(user4 , book2)
+    library1.takeBook(user4, book2)
     println("5. All books = ${library1.getAllBookStatuses()}")
 
     library1.unregisterUser(user4) // book2 will be available again
     println("6. All books = ${library1.getAllBookStatuses()}")
-
 }
